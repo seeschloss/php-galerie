d267ef5e5300c5deacb03dd6a60b670997ad0be2
SeeSchloss <see@seos.fr>
2022-06-13 14:50:50 +0000

    Ready for use, with sensible cli options

--- php-galerie.php
+++ php-galerie.php
@@ -2,33 +2,41 @@
 
 class Photo {
 	public $original_path = "";
-	public $dynamic = false;
 
 	public $files = [];
 
-	function __construct($path, $dynamic = false) {
+	function __construct($path) {
 		$this->original_path = $path;
-		$this->dynamic = $dynamic;
 	}
 
 	function path_original() {
 		return $this->original_path;
 	}
 
-	function path_size($width, $height) {
+	function path_size($width, $height, $crop = false) {
 		$cache_directory = dirname($this->original_path)."/.cache";
+
+		if (!file_exists($cache_directory)) {
+			mkdir($cache_directory, 0777, true);
+		}
 
 		$filename = basename($this->original_path);
 		$width = (int)$width;
 		$height = (int)$height;
 
-		$path_size = "{$cache_directory}/{$filename},w={$width},h={$height}";
+		$path_size = "{$cache_directory}/w={$width},h={$height}";
+
+		if ($crop) {
+			$path_size .= ",c";
+		}
+
+		$path_size .= ",{$filename}";
 
 		return $path_size;
 	}
 
-	function generate_size($width, $height) {
-		if (!file_exists($this->path_size($width, $height))) {
+	function generate_size($width, $height, $crop = false) {
+		if (!file_exists($this->path_size($width, $height, $crop))) {
 			$cache_directory = dirname($this->original_path)."/.cache";
 			if (!file_exists($cache_directory)) {
 				mkdir($cache_directory);
@@ -40,36 +48,48 @@ class Photo {
 
 			list($width_orig, $height_orig) = getimagesize($this->original_path);
 
-			$ratio_orig = $width_orig/$height_orig;
+			if ($crop) {
+				$smallest_edge = min($width_orig, $height_orig);
 
-			$width_proportional = $width;
-			$height_proportional = $height;
-
-			if ($width/$height > $ratio_orig) {
-				$width_proportional = $height * $ratio_orig;
+				$r_resized = imagecreatetruecolor($width, $height);
+				imagecopyresampled($r_resized, $r, 0, 0, ($width_orig - $smallest_edge)/2, ($height_orig - $smallest_edge)/2, $width, $height, $smallest_edge, $smallest_edge);
 			} else {
-				$height_proportional = $width / $ratio_orig;
+				$ratio_orig = $width_orig/$height_orig;
+
+				$width_proportional = $width;
+				$height_proportional = $height;
+
+				if ($width/$height > $ratio_orig) {
+					$width_proportional = $height * $ratio_orig;
+				} else {
+					$height_proportional = $width / $ratio_orig;
+				}
+
+				$r_resized = imagecreatetruecolor($width_proportional, $height_proportional);
+				imagecopyresampled($r_resized, $r, 0, 0, 0, 0, $width_proportional, $height_proportional, $width_orig, $height_orig);
 			}
 
-			$r_resized = imagecreatetruecolor($width_proportional, $height_proportional);
-			imagecopyresampled($r_resized, $r, 0, 0, 0, 0, $width_proportional, $height_proportional, $width_orig, $height_orig);
-
-			imagejpeg($r_resized, $this->path_size($width, $height), 95);
+			imagejpeg($r_resized, $this->path_size($width, $height, $crop), 95);
 		}
 
-		return $this->path_size($width, $height);
+		return file_get_contents($this->path_size($width, $height, $crop));
 	}
 
 	function url_original() {
-		return $this->dynamic ? "?image=".$this->original_path : 'files/'.basename($this->original_path);
+		return basename($this->original_path);
 	}
 
-	function url_size($width, $height) {
-		if ($width < 300 and $height < 300) {
-			return "data:image/jpeg;base64,".base64_encode(file_get_contents($this->path_size($width, $height)));
+	function url_size($width, $height, $crop = false, $embed = false) {
+		if ($embed) {
+			return "data:image/jpeg;base64,".base64_encode($this->generate_size($width, $height, $crop));
+		} else {
+			$url_size = ".cache/w={$width},h={$height}";
+			if ($crop) {
+				$url_size .= ",c";
+			}
+			$url_size .= ",".basename($this->original_path);
+			return $url_size;
 		}
-
-		return $this->dynamic ? "?image={$this->original_path}&w={$width}&h={$height}" : "files/w={$width},h={$height},".basename($this->original_path);
 	}
 
 	function classes() {
@@ -102,30 +122,22 @@ class Photo {
 		return $title;
 	}
 
-	function html_thumbnail($width, $height) {
+	function html_thumbnail($width, $height, $crop_thumbnail = false, $embed_thumbnail = false) {
 		$html = <<<HTML
 	<figure class="{$this->classes()}">
-		<a href="{$this->url_original()}"><img src="{$this->url_size($width, $height)}" /></a>
+		<a href="{$this->url_original()}"><img src="{$this->url_size($width, $height, $crop_thumbnail, $embed_thumbnail)}" /></a>
 		<figcaption><a href="{$this->url_original()}">{$this->title()}</a></figcaption>
 	</figure>
 
 HTML;
 
-		$this->files['files/'.basename($this->original_path)] = $this->path_original();
+		$this->files[basename($this->original_path)] = ['path' => $this->path_original()];
 
-		//$this->files['files/w=250,h=250,'.basename($this->original_path)] = $this->path_size(250, 250);
-
-		return $html;
-	}
-
-	function files() {
-		$files = [];
-
-		foreach ($this->files as $html_path => $filesystem_path) {
-			$files[$html_path] = file_get_contents($filesystem_path);
+		if (!$embed_thumbnail) {
+			$this->files[".cache/w={$width},h={$height},".basename($this->original_path)] = ['data' => $this->generate_size($width, $height, $crop_thumbnail)];
 		}
 
-		return $files;
+		return $html;
 	}
 }
 
@@ -140,11 +152,28 @@ class Gallery {
 
 	public $html;
 
-	static function is_gallery($index) {
-		$lines = file($index);
+	public $embed_thumbnails = false;
+	public $crop_thumbnails = false;
 
-		if (substr($lines[1], 0, 24) == '<html class="php-galerie') {
+
+	public $thumbnail_width = 250;
+	public $thumbnail_height = 250;
+
+	static function is_gallery($directory) {
+		if (file_exists($directory."/index.html")) {
+			$lines = file($directory."/index.html");
+
+			if (substr($lines[1], 0, 24) == '<html class="php-galerie') {
+				return true;
+			}
+		} else if (file_exists($directory."/index.php")) {
 			return true;
+		} else {
+			$files = glob($directory."/*.jpg") + glob($directory."/*.JPG") + glob($directory."/*.jpeg") + glob($directory."/*.JPEG");
+
+			if (count($files)) {
+				return true;
+			}
 		}
 
 		return false;
@@ -166,7 +195,7 @@ HTML;
 		return $html;
 	}
 
-	function index($index = null) {
+	function read_index($index = null) {
 		$lines = file($index);
 
 		if (substr($lines[1], 0, 24) == '<html class="php-galerie') {
@@ -184,68 +213,76 @@ HTML;
 		}
 	}
 
-	function directory($directory) {
-		$files = [];
+	function read_directory($directory, $recursive = false, $max_depth = NULL) {
+		$this->url = basename($directory);
+		$this->title = $this->url;
+		$photos = [];
 
 		foreach (glob($directory."/*.jpg") + glob($directory."/*.JPG") + glob($directory."/*.jpeg") + glob($directory."/*.JPEG") as $file) {
-			if (strpos(basename($file), ".") !== 0) {
-				$files[] = $file;
+			if (strpos(basename($file), ".") !== 0 and strpos(basename($file), "_") !== 0) {
+				$photos[$file] = new Photo($file);
 			}
 		}
 
-		$this->files_raw = $files;
+		$this->photos = $photos;
+		if (count($this->photos)) {
+			$this->thumbnail = $this->photos[array_key_first($this->photos)]->url_size($this->thumbnail_width, $this->thumbnail_height, $this->crop_thumbnails, true);
+		}
 
 		$galleries = [];
 
-		foreach (glob($directory."/*/index.html") as $index) {
-			if (Gallery::is_gallery($index)) {
-				$gallery = new Gallery();
-				$gallery->index($index);
-				$galleries[] = $gallery;
+		if ($recursive) {
+			if ($max_depth === NULL or $max_depth > 1) {
+				foreach (glob($directory."/*/") as $subdirectory) {
+					if (strpos(basename($subdirectory), ".") !== 0 and strpos(basename($subdirectory), "_") !== 0) {
+						if (Gallery::is_gallery($subdirectory)) {
+							$gallery = new Gallery();
+							$gallery->read_directory($subdirectory, $recursive, $max_depth - 1);
+							$galleries[] = $gallery;
+						}
+					}
+				}
 			}
 		}
 
 		$this->galleries = $galleries;
-	}
 
-	function prepare_files($dynamic) {
-		$photos = [];
-
-		foreach ($this->files_raw as $file) {
-			$photos[$file] = new Photo($file, $dynamic);
+		if (file_exists($directory."/index.html")) {
+			// This will show subdirectories which have an index.html (and their thumbnail) even if recursivity is disabled.
+			$this->read_index($directory."/index.html");
 		}
-
-		$this->photos = $photos;
 	}
 
 	function thumbnail_base64() {
 		foreach ($this->photos as $photo) {
-			return "data:image/jpeg;base64,".base64_encode(file_get_contents($photo->path_size(250, 250)));
+			return "data:image/jpeg;base64,".base64_encode($photo->generate_size($this->thumbnail_width, $this->thumbnail_height, $this->crop_thumbnails));
 		}
 
 		return "";
 	}
 
-	function html($dynamic = false) {
-		$this->prepare_files($dynamic);
-
-		$html = <<<HTML
-<!DOCTYPE html>
-<html class="php-galerie" data-thumbnail-src="{$this->thumbnail_base64()}">
-<head>
-	<style>
+	function html() {
+		$css = <<<CSS
 		body {
 			display: grid;
 			gap: 0.5em;
-			grid-template-columns: repeat(auto-fit, 250px);
+			grid-template-columns: repeat(auto-fit, {$this->thumbnail_width}px);
 			grid-auto-flow: dense;
 			background-color: black;
+			color: white;
+			/*
+			column-count: auto;
+			column-width: 250px;
+			column-gap: 5px;
+			*/
 		}
 
 		figure {
-			margin: 0;
+			margin: 0 0 5px 0;
 			border: 2px white solid;
 			border-radius: 2px;
+			break-inside: avoid;
+			background-color: #555;
 		}
 
 		figure.has-title a {
@@ -255,14 +292,10 @@ HTML;
 		figure > a {
 			font-size: 0;
 			display: block;
-		}
-
-		figure > a img {
-			width: 100%;
+			text-align: center;
 		}
 
 		figure figcaption {
-			background-color: #555;
 			color: #fff;
 			font: italic smaller sans-serif;
 			text-align: center;
@@ -278,72 +311,266 @@ HTML;
 			text-decoration: none;
 			display: block;
 		}
+CSS;
+
+		if ($this->crop_thumbnails) {
+			$css .= <<<CSS
+		figure > a img {
+			width: 100%;
+		}
+CSS;
+		} else {
+			$css .= <<<CSS
+		figure > a img {
+			max-width: 100%;
+		}
+CSS;
+		}
+
+		$html = <<<HTML
+<!DOCTYPE html>
+<html class="php-galerie" data-thumbnail-src="{$this->thumbnail_base64()}">
+<head>
+	<style>
+		{$css}
 	</style>
 </head>
 <body>
 HTML;
 
 		foreach ($this->galleries as $gallery) {
-			$html .= $gallery->html_thumbnail(250, 250);
+			$html .= $gallery->html_thumbnail($this->thumbnail_width, $this->thumbnail_height);
 		}
 
 		foreach ($this->photos as $photo) {
-			$html .= $photo->html_thumbnail(250, 250);
+			$html .= $photo->html_thumbnail($this->thumbnail_width, $this->thumbnail_height, $this->crop_thumbnails, $this->embed_thumbnails);
 		}
 
 		$html .= "</body></html>";
 
 		return $html;
 	}
-}
 
-if (isset($_REQUEST['image'])) {
-	$photo = new Photo($_REQUEST['image']);
-	header("Content-Type: image/jpeg");
-	if (isset($_REQUEST['w']) and isset($_REQUEST['h'])) {
-		$photo->generate_size($_REQUEST['w'], $_REQUEST['h']);
-		readfile($photo->path_size($_REQUEST['w'], $_REQUEST['h']));
-	} else {
-		readfile($photo->path_original());
-	}
-	die();
-} else {
-	$options = getopt('o:i:', ['output-directory::', 'input-directory::']);
+	function write($output_directory, $recursive = false, $max_depth = NULL) {
+		if ($max_depth !== NULL and $max_depth <= 0) {
+			return;
+		}
 
-	$input_directory = '.';
-	if (!empty($options['i'])) {
-		$input_directory = $options['i'];
-	}
-	if (!empty($options['input-directory'])) {
-		$input_directory = $options['input-directory'];
-	}
-
-	$output_directory = null;
-	if (!empty($options['o'])) {
-		$output_directory = $options['o'];
-	}
-	if (!empty($options['output-directory'])) {
-		$output_directory = $options['output-directory'];
-	}
-
-	$gallery = new Gallery();
-	$gallery->directory($input_directory);
-
-	if ($output_directory) {
 		if (!file_exists($output_directory)) {
 			mkdir($output_directory, 0777, true);
 		}
-		file_put_contents($output_directory."/index.html", $gallery->html());
-		foreach ($gallery->photos as $photo) {
-			foreach ($photo->files() as $relative_path => $contents) {
+		Log::stderr("Writing gallery '{$output_directory}'");
+		file_put_contents($output_directory."/index.html", $this->html());
+		Log::stderr(".");
+		foreach ($this->photos as $photo) {
+			foreach ($photo->files as $relative_path => $file) {
 				$directory = dirname($output_directory."/".$relative_path);
 				if (!file_exists($directory)) {
 					mkdir($directory, 0777, true);
 				}
-				file_put_contents($output_directory."/".$relative_path, $contents);
+				$file_output_path = $output_directory."/".$relative_path;
+				if (file_exists($output_directory."/".$relative_path)) {
+					if (filemtime($photo->path_original()) > filemtime($file_output_path)) {
+						if (isset($file['path']) and file_exists($file['path'])) {
+							copy($file['path'], $file_output_path);
+							Log::stderr("+");
+						} else if (isset($file['data'])) {
+							file_put_contents($file_output_path, $file['data']);
+							Log::stderr("+");
+						}
+					} else {
+						Log::stderr("=");
+					}
+				} else {
+					if (isset($file['path']) and file_exists($file['path'])) {
+						copy($file['path'], $file_output_path);
+						Log::stderr(".");
+					} else if (isset($file['data'])) {
+						file_put_contents($file_output_path, $file['data']);
+						Log::stderr(".");
+					}
+				}
 			}
 		}
+		Log::stderr("\n");
+
+		if ($recursive) {
+			foreach ($this->galleries as $gallery) {
+				$gallery->write($output_directory.'/'.$gallery->url, $recursive, $max_depth - 1);
+			}
+		}
+	}
+}
+
+class Log {
+	static function stderr($message) {
+		file_put_contents('php://stderr', $message);
+	}
+}
+
+function help_message($options) {
+	$longest_switches_line_lenght = 0;
+	foreach ($options as $option) {
+		$switches = array_shift($option['description']);
+		$longest_switches_line_lenght = max($longest_switches_line_lenght, strlen($switches));
+	}
+
+	echo <<<TXT
+php-galerie
+Generate a static HTML photo gallery from directories containing JPEG files.
+
+Options:
+
+TXT;
+	foreach ($options as $option) {
+		$switches = str_pad(array_shift($option['description']), $longest_switches_line_lenght, " ");
+		$first_line = array_shift($option['description']);
+		echo "  {$switches} {$first_line}\n";
+		foreach ($option['description'] as $line) {
+			$padding = str_pad("", $longest_switches_line_lenght, " ");
+			echo "  {$padding}     {$line}\n";
+		}
+	}
+}
+
+if (php_sapi_name() == 'cli') {
+	$options = [
+		'help' => [
+			'short' => 'h',
+			'long' => 'help',
+			'description' => ['-h, --help', 'Show this help message'],
+		],
+		'output' => [
+			'short' => 'o:',
+			'long' => 'output-directory::',
+			'description' => ['-o <directory>, --output-directory=<directory>', 'Output directory', '(current directory by default)'],
+		],
+		'input' => [
+			'short' => 'i:',
+			'long' => 'input-directory::',
+			'description' => ['-i <directory>, --input-directory=<directory>', 'Input directory', '(current directory by default)'],
+		],
+		'recursive' => [
+			'short' => 'r',
+			'long' => 'recursive',
+			'description' => ['-r, --recursive', 'Follow subdirectories recursively'],
+		],
+		'max-depth' => [
+			'short' => 'd:',
+			'long' => 'max-depth::',
+			'description' => ['-d <depth>, --max-depth=<max-depth>', 'Maximum recursive depth, implies --recursive', '0: no limit', '1: no recursivity', '(no limit by default)'],
+		],
+		'embed-thumbnails' => [
+			'short' => 'e',
+			'long' => 'embed-thumbnails',
+			'description' => ['-e, --embed-thumbnails', 'Embed thumbnails in index.html'],
+		],
+		'thumbnail-size' => [
+			'short' => 't:',
+			'long' => 'thumbnail-size::',
+			'description' => ['-t <width>x<height>, --thumbnail-size=<width>x<height>', 'Resize thumbnails to size', '(250 x 250 by default)'],
+		],
+		'crop' => [
+			'short' => 'c',
+			'long' => 'crop',
+			'description' => ['-c, --crop', 'Crop thumbnails to fill size'],
+		],
+	];
+
+	$short_options = "";
+	$long_options = [];
+	foreach ($options as $option) {
+		$short_options .= $option['short'];
+		$long_options[] = $option['long'];
+	}
+	$cmdline_options = getopt($short_options, $long_options);
+
+	if (isset($cmdline_options['h']) or isset($cmdline_options['help'])) {
+		help_message($options);
+		exit(0);
+	}
+
+	$input_directory = '.';
+	if (!empty($cmdline_options['i'])) {
+		$input_directory = $cmdline_options['i'];
+	}
+	if (!empty($cmdline_options['input-directory'])) {
+		$input_directory = $cmdline_options['input-directory'];
+	}
+
+	$output_directory = null;
+	if (!empty($cmdline_options['o'])) {
+		$output_directory = $cmdline_options['o'];
+	}
+	if (!empty($cmdline_options['output-directory'])) {
+		$output_directory = $cmdline_options['output-directory'];
+	}
+
+	$recursive = false;
+	if (isset($cmdline_options['r']) or isset($cmdline_options['recursive'])) {
+		$recursive = true;
+	}
+
+	$max_depth = NULL;
+	if (isset($cmdline_options['d'])) {
+		$max_depth = $cmdline_options['d'];
+		$recursive = true;
+	}
+	if (isset($cmdline_options['max-depth'])) {
+		$max_depth = $cmdline_options['max-depth'];
+		$recursive = true;
+	}
+
+	$embed_thumbnails = false;
+	if (isset($cmdline_options['e']) or isset($cmdline_options['embed-thumbnails'])) {
+		$embed_thumbnails = true;
+	}
+
+	$crop_thumbnails = false;
+	if (isset($cmdline_options['c']) or isset($cmdline_options['crop'])) {
+		$crop_thumbnails = true;
+	}
+
+	$thumbnail_size = "250x250";
+	if (isset($cmdline_options['t'])) {
+		$thumbnail_size = $cmdline_options['t'];
+	}
+	if (isset($cmdline_options['thumbnail-size'])) {
+		$thumbnail_size = $cmdline_options['thumbnail-size'];
+	}
+
+	if (strpos($thumbnail_size, 'x') === false) {
+		var_dump($thumbnail_size);
+		help_message($options);
+		exit(1);
+	}
+
+	list($thumbnail_width, $thumbnail_height) = explode('x', $thumbnail_size);
+
+	if ((int)$thumbnail_width != $thumbnail_width or (int)$thumbnail_height != $thumbnail_height or $thumbnail_width == 0 or $thumbnail_height == 0) {
+		help_message($options);
+		exit(1);
+	}
+
+	$gallery = new Gallery();
+	$gallery->embed_thumbnails = $embed_thumbnails;
+	$gallery->crop_thumbnails = $crop_thumbnails;
+	$gallery->thumbnail_width = $thumbnail_width;
+	$gallery->thumbnail_height = $thumbnail_height;
+	$gallery->read_directory($input_directory, $recursive, $max_depth);
+
+	if ($output_directory) {
+		$gallery->write($output_directory, $recursive, $max_depth);
+	}
+} else {
+	$request_path_relative = '.'.urldecode(str_replace(dirname($_SERVER['SCRIPT_NAME']), '', $_SERVER['REQUEST_URI']));
+	if (file_exists($request_path_relative) and !is_dir($request_path_relative)) {
+		$photo = new Photo($request_path_relative);
+		header("Content-Type: image/jpeg");
+		readfile($request_path_relative);
 	} else {
+		$gallery = new Gallery();
+		$gallery->read_directory($request_path_relative);
 		echo $gallery->html(true);
 	}
 }
